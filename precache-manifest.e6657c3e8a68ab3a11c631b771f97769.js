39bade54ee44cdda7ef12c6e98f1faf38bc84460
JaKris <crist_gui@hotmail.com>
2020-08-20 12:54:49 +0200

    Updates

---
+++ precache-manifest.e6657c3e8a68ab3a11c631b771f97769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b43f59f337a09b930d1784d3ebb93a",
+    "url": "/AdministradorCitas/index.html"
+  },
+  {
+    "revision": "541d2f69e1e101dd5658",
+    "url": "/AdministradorCitas/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "02c528cb543c24f923db",
+    "url": "/AdministradorCitas/static/js/2.cb745f93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AdministradorCitas/static/js/2.cb745f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541d2f69e1e101dd5658",
+    "url": "/AdministradorCitas/static/js/main.680b21e4.chunk.js"
+  },
+  {
+    "revision": "4d1afd88323d9a18b4c0",
+    "url": "/AdministradorCitas/static/js/runtime-main.2183898f.js"
+  }
+]);
